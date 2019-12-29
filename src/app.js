b6ea18d6dda7b0322bd76c5cc674e0f5ea4f1a24
Miras Kenzhegaliyev <miras.kenzhegaliyev@nu.edu.kz>
2019-12-29 12:04:51 +0200

    Complete lesson 13

---
+++ src/app.js
@@ -0,0 +1,40 @@
+console.log("App.js is running");
+
+// JSX - Javascript XML
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in hands of a computer",
+    options: ["One", "Two"]
+} 
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options && app.options.length > 0 ? "Here are your options" : "No options"}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const user = {
+    name: "Miras",
+    age: "22",
+    location: "Tartu"
+}
+
+function getLocation(location) {
+    if (location) return <p>Location: {location}</p>
+}
+const templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : "Anonymous"}</h1>
+        {(user.age && user.age >=18) && <p>Age: {user.age} </p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,19 @@
+var nameVar = 'Andrew';
+var nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Len';
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
+
+var fullName = 'Andrew Mead';
+
+if (fullName) {
+    const firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
